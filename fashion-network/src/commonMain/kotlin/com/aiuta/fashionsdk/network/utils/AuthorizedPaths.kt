c2790c68a6ab621da7a4376dff5af8e8fd328a5a
MishkaV <mvorozhtsov.work@gmail.com>
2026-02-20 18:58:02 +0100

    Fix auth path for JWT

--- fashion-network/src/commonMain/kotlin/com/aiuta/fashionsdk/network/utils/AuthorizedPaths.kt
+++ fashion-network/src/commonMain/kotlin/com/aiuta/fashionsdk/network/utils/AuthorizedPaths.kt
@@ -1,5 +1,7 @@
 package com.aiuta.fashionsdk.network.utils
 
+import com.aiuta.fashionsdk.network.internal.KtorHttpClientFactory
+
 internal val authorizedPaths = setOf(
-    "/sku_images_operations",
+    "${KtorHttpClientFactory.DEFAULT_ENCODED_PATH}sku_images_operations",
 )
