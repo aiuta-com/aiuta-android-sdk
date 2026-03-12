c399effce9173a0e216bd915e82976e7590641c3
Mishka Vorozhtsov <54765046+MishkaV@users.noreply.github.com>
2026-03-12 12:35:23 +0300

    [BREAKING CHANGES] Migrate to Aiuta backend for size fit (#590)
    * Migrate to Aiuta backend for size fit
    
    * Update api dump
    
    * Update secrets

--- fashion-sizefit-compose/src/commonMain/kotlin/com/aiuta/fashionsdk/sizefit/compose/ui/internal/navigation/SizeFitNavigationInitialisation.kt
+++ fashion-sizefit-compose/src/commonMain/kotlin/com/aiuta/fashionsdk/sizefit/compose/ui/internal/navigation/SizeFitNavigationInitialisation.kt
@@ -2,18 +2,22 @@ package com.aiuta.fashionsdk.sizefit.compose.ui.internal.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.remember
 import com.aiuta.fashionsdk.configuration.AiutaConfiguration
 import com.aiuta.fashionsdk.internal.navigation.AiutaNavigationInitialisation
 import com.aiuta.fashionsdk.internal.navigation.aiutaEntryProvider
 import com.aiuta.fashionsdk.sizefit.compose.ui.internal.composition.LocalAiutaSizeFit
-import com.aiuta.fashionsdk.sizefit.core.AiutaSizeFit
+import com.aiuta.fashionsdk.sizefit.core.sizeFit
 
 @Composable
 internal fun SizeFitNavigationInitialisation(
     aiutaConfiguration: AiutaConfiguration,
-    aiutaSizeFit: AiutaSizeFit,
     content: @Composable () -> Unit,
 ) {
+    val aiutaSizeFit = remember {
+        aiutaConfiguration.aiuta.sizeFit
+    }
+
     // Base init
     AiutaNavigationInitialisation(
         startScreen = SizeFitScreen.Questionary,
